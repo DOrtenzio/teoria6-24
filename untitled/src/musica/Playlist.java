7c9470095d6d494964b6013476a34a83be88cdd9
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-10-24 18:30:16 +0200

    Update Playlist.java

--- untitled/src/musica/Playlist.java
+++ untitled/src/musica/Playlist.java
@@ -4,17 +4,22 @@ public class Playlist {
     private Canzone [] miaPlaylist;
     private int inserite;
 
+    //GET E SET
     public Canzone[] getMiaPlaylist() { return miaPlaylist; }
     public void setMiaPlaylist(Canzone[] miaPlaylist) { this.miaPlaylist = miaPlaylist; }
 
+    //COSTRUTTORE
     public Playlist(){
         this.miaPlaylist=new Canzone[100];
         this.inserite=0;
     }
 
+    /*METODI*/
+
+    //INSERIMENTI
     public int getNumeroCanzoniInserite(){ return this.inserite; }
     public boolean aggiungiCanzone(Canzone canzoneNuova){
-        if (this.inserite==this.miaPlaylist.length)
+        if (this.inserite==this.miaPlaylist.length) //Controllo disponibilità spazio nell'array
             return false;
         else{
             miaPlaylist[this.inserite]=canzoneNuova;
@@ -23,7 +28,7 @@ public class Playlist {
         }
     }
     public boolean aggiungiCanzone(String nomeCanzone,String nomeArtista,int tempo){
-        if (this.inserite==this.miaPlaylist.length)
+        if (this.inserite==this.miaPlaylist.length) //Controllo disponibilità spazio nell'array
             return false;
         else{
             miaPlaylist[this.inserite]=new Canzone(nomeCanzone,nomeArtista,tempo);
@@ -31,23 +36,28 @@ public class Playlist {
             return true;
         }
     }
+
+    //CANCELLAZIONE
     public boolean cancellaCanzone(Canzone cancellare){
-        int posizione=-1;
-        for (int i=0;i<this.inserite;i++){
+        int posizione=-1; //POSIZIONE IN CUI SALVO LA STESSA DELLA CANZONE RICERCATA IN miaPlaylist[]
+        for (int i=0;i<this.inserite;i++){ //Ricerca
             if (cancellare.getNomeCanzone().equals(miaPlaylist[i].getNomeCanzone())&&cancellare.getNomeArtista().equals(miaPlaylist[i].getNomeArtista())&&cancellare.getTempo()==miaPlaylist[i].getTempo()){
                 posizione=i;
                 break;
             }
         }
-        if (posizione==-1)
+        if (posizione==-1) //Se non ho trovato la canzone cercata
             return false;
-        else{
+        else{ //Parte la cancellazione a slittaggio indietro di un posto fino alla posizione dell'elemento da cancellare
             for (int l=posizione;l<this.inserite-1;l++){
                 miaPlaylist[l]=miaPlaylist[l+1];
             }
+            this.inserite--;
             return true;
         }
     }
+
+    //RICERCA CANZONI E CANTANTI in base all'una o all'altra
     public String ricercaCantante(String titolo){
         for (int i=0;i<this.inserite;i++){
             if (titolo.equals(miaPlaylist[i].getNomeCanzone()))
@@ -66,14 +76,16 @@ public class Playlist {
         }
         Canzone [] canzoniArtista=new Canzone[ins];
         for (int l=0;l<ins;l++){
-            canzoniArtista[l]=new Canzone(miaPlaylist[posizioni[l]].getNomeCanzone(),miaPlaylist[posizioni[l]].getNomeArtista(),miaPlaylist[posizioni[l]].getTempo());
+            canzoniArtista[l]=miaPlaylist[posizioni[l]];
         }
         return canzoniArtista;
     }
+
+    //STAMPA
     public String toString(){
         String s="[ ";
         for (int i=0;i<this.inserite;i++){
-            s=s+this.miaPlaylist[i].toString()+" , ";
+            s=s+" // "+this.miaPlaylist[i].toString();
         }
         s=s+" ]";
         return s;
